a4246e48af0a72004db7d7dcf85ceeec240f9d7b
Anton Zagrebelny <anton@stigg.io>
2022-10-19 09:46:58 +0300

    chore: demo app api key

--- components/PricingPlans.tsx
+++ components/PricingPlans.tsx
@@ -1,9 +1,7 @@
-import {Paywall, Plan, StiggProvider, useStiggContext} from "@stigg/react-sdk";
-import {useEffect, useState} from "react";
+import {Paywall, Plan, StiggProvider} from "@stigg/react-sdk";
 
-const STIGG_CLIENT_API_KEY = "$2b$10$AyLF3/ctHts4M5GnlETDH.4maZUCxzz1JQFxIsyVBoxciRUbPULHi:e7b8aabd-64b8-436c-902b-bf81a6025cf0";
-const STIGG_CUSTOMER_ID = 'p2mtkzYJTLHaCuAS965si6';
-
+// Using the demo app API key, replace with your own client API key
+const STIGG_CLIENT_API_KEY = "$2b$10$ouA6M7FOJSxHH/3mkjZnZOJf5lUmFPvj8G0h1zIQxRkOY9CrpY53.:248ec79b-0932-4157-b514-30375bf5eeb8";
 
 export const PricingPlans = () => {
   return (
@@ -14,7 +12,6 @@ export const PricingPlans = () => {
           planMaxWidth: '360px'
         }
       }}
-      customerId={STIGG_CUSTOMER_ID}
       apiKey={STIGG_CLIENT_API_KEY}>
       <Paywall onPlanSelected={({ plan }: { plan: Plan }) => {
         console.log(`Selected plan: ${plan.displayName}`);
